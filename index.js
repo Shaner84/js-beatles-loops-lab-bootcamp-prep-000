7ee4146200db9c00aed28451b7fc1db16aed9b59
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-12 05:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var all = []
+  for (var i=0; i < musicians.length; i++){
+    all.push(musicians[i] + " plays " + instruments[i])
+}
+return all
+}
+
+function johnLennonFacts(facts){
+ const newFacts = [];
+ 
+ let i = 0;
+  while (i < facts.length){
+     newFacts.push(facts[i] +"!!!");
+        i++;
+  }
+return newFacts;
+}
+
+function iLoveTheBeatles(n){
+  
+}
